2d1f5a39cb6d1ba5aa47e39de4f51bf75171c676
Gernot Raudner <office@gernotraudner.at>
2016-07-05 21:23:09 +0200

    bootstrap fun!

--- src/scripts/NavBar.js
+++ src/scripts/NavBar.js
@@ -1,10 +1,10 @@
 var NavBar = React.createClass({
     render: function render(){
         return (
-            <nav>
-                <Link to="/list" activeClassName="active">Rezepte</Link>
-                <Link to="/random"  activeClassName="active">Zufallsrezepte</Link>
-                <Link to="/search"  activeClassName="active">Zutatensuche</Link>
+            <nav className="btn-group btn-group-justified">
+                <Link to="/list" activeClassName="active" className="btn btn-default">Rezepte</Link>
+                <Link to="/random"  activeClassName="active" className="btn btn-default">Zufallsrezepte</Link>
+                <Link to="/search"  activeClassName="active" className="btn btn-default">Zutatensuche</Link>
             </nav>
         );
     }
